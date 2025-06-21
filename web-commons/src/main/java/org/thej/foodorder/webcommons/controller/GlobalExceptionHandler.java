69d078232e13c3e3f9c5fb98eb6140821fdfc13a
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-06-21 22:18:54 +0530

    Global exception handler

---
+++ web-commons/src/main/java/org/thej/foodorder/webcommons/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.thej.foodorder.webcommons.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.thej.foodorder.commons.exception.CustomException;
+import org.thej.foodorder.webcommons.dto.response.ApiResponse;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleCustomException(CustomException ex) {
+        ApiResponse<List<CustomException.CustomExceptionDetails>> response = new ApiResponse<>(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.getErrors());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        ApiResponse<String> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred", null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
