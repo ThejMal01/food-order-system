a15f312210fc09571970ed7851a554d054447e8b
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-06-21 21:07:34 +0530

    request routing setup

---
+++ api-gateway/src/main/java/org/thej/foodorder/apigateway/config/GatewayConfig.java
@@ -0,0 +1,18 @@
+package org.thej.foodorder.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth", r -> r.path("/auth/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://AUTH"))
+                .build();
+    }
+}
---
+++ api-gateway/src/main/java/org/thej/foodorder/apigateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.thej.foodorder.apigateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.*;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                );
+
+        return http.build();
+    }
+}
