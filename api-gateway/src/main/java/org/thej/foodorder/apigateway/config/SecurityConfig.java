acce6779a827e17b334d2b80dfc5c0ed5e991b57
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-06-29 01:26:52 +0530

    Added jwt filter

--- api-gateway/src/main/java/org/thej/foodorder/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/thej/foodorder/apigateway/config/SecurityConfig.java
@@ -5,14 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.thej.foodorder.apigateway.filter.JwtAuthFilter;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         http
@@ -21,7 +26,8 @@ public class SecurityConfig {
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
                         .anyExchange().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, SecurityWebFiltersOrder.AUTHENTICATION);
 
         return http.build();
     }
