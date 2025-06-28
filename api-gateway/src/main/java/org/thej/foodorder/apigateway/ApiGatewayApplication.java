427b82d7db291fe4ade74000bab3cbdbbd4ea063
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-06-29 01:25:20 +0530

    Configured component scan

--- api-gateway/src/main/java/org/thej/foodorder/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/thej/foodorder/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,14 @@ package org.thej.foodorder.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {
+        "org.thej.foodorder.master",
+        "org.thej.foodorder.webcommons",
+        "org.thej.foodorder.apigateway",})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
