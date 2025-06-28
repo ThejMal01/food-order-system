004cddbf84bae609723e5e196484cbe70d17667e
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-06-29 01:28:23 +0530

    Configured jwt auth filter

--- api-gateway/src/main/java/org/thej/foodorder/apigateway/filter/JwtAuthFilter.java
+++ api-gateway/src/main/java/org/thej/foodorder/apigateway/filter/JwtAuthFilter.java
@@ -1,18 +1,61 @@
-package org.thej.foodorder.webcommons.filter;
+package org.thej.foodorder.apigateway.filter;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import org.thej.foodorder.webcommons.service.JwtService;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @Component
-public class JwtAuthFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+@RequiredArgsConstructor
+public class JwtAuthFilter implements WebFilter {
 
+    private final JwtService jwtService;
+
+    @Override
+    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        if (jwtService.isTokenExpired(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String username = jwtService.extractUsername(token);
+
+        // Extract role from token and set up authorities
+        Object role = jwtService.extractRoles(token);
+        List<SimpleGrantedAuthority> authorities = Collections.emptyList();
+
+        if (role != null) {
+            authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()));
+        }
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                new User(username, "", authorities), null, authorities);
+
+        return chain.filter(exchange)
+                .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(new SecurityContextImpl(auth))));
     }
+
 }
