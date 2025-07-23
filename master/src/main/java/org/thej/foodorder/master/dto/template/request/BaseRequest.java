ee0ac92de2da7adf6933edeed5772b97e2fc4d85
ThejMal01 <thejani.mallawaarachchi@gmail.com>
2025-07-23 10:35:21 +0530

    token exchange

--- master/src/main/java/org/thej/foodorder/master/dto/template/request/BaseRequest.java
+++ master/src/main/java/org/thej/foodorder/master/dto/template/request/BaseRequest.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class BaseRequest {
     private String requestId;
     private String timestamp;
-
     public BaseRequest() {
         this.requestId = generateRequestId();
         this.timestamp = getCurrentTimestamp();
