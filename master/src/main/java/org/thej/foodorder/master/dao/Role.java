1890fc7ac216da306d6d90be12ffc518c084206a
Silverviles <sudarakatharindu2001@gmail.com>
2025-07-24 11:50:44 +0530

    fix: login response infinite recursive serialization error fix

--- master/src/main/java/org/thej/foodorder/master/dao/Role.java
+++ master/src/main/java/org/thej/foodorder/master/dao/Role.java
@@ -1,5 +1,6 @@
 package org.thej.foodorder.master.dao;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,15 +17,18 @@ import java.util.Collection;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
     private String roleName;
     private String roleDescription;
     @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
     private Collection<User> users;
     @ManyToMany
     @JoinTable(
         name = "t_role_privilege",
         joinColumns = @JoinColumn(name = "role_id"),
         inverseJoinColumns = @JoinColumn(name = "privilege_id"))
+    @JsonIgnore
     private Collection<Privilege> privileges;
 }
